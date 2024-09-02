3026eb70d48f7e9e6c68be29292cc7eb61eab355
Lepros311 <andy.strickland@gmail.com>
2024-09-02 16:16:36 -0400

    Add functionality

--- Program.cs
+++ Program.cs
@@ -1,113 +1,197 @@
 ﻿using Spectre.Console;
+using System;
+using System.Text;
+using System.Collections.Generic;
 
-var race1 = new Race("Indianapolis 500", "May 25, 2025", "Indianapolis Motor Speedway", "");
-var race2 = new Race("24 Hours of Le Mans", "June 11, 2025", "Circuit de la Sarthe", "");
-var race3 = new Race("Daytona 500", "February 16, 2025", "Indianapolis Motor Speedway", "");
-
-var driver1 = new Driver("Al Unser Jr.");
-var driver2 = new Driver("Ricardo Patrese");
-var driver3 = new Driver("Erik Carlsson");
-var driver4 = new Driver("Craig Breedlove");
-var driver5 = new Driver("Louis Meyer");
-var driver6 = new Driver("Rauno Aaltonen");
-var driver7 = new Driver("Giuseppe Farina");
-var driver8 = new Driver("Richard Burns");
-var driver9 = new Driver("David Pearson");
-var driver10 = new Driver("Jean Behra");
-var driver11 = new Driver("Wolfgang von Trips");
-var driver12 = new Driver("Henry Segrave");
-var driver13 = new Driver("Gerhard Berger");
-var driver14 = new Driver("David Coulthard");
-var driver15 = new Driver("Carlos Reutemann");
-var driver16 = new Driver("Didier Pironi");
-var driver17 = new Driver("Robert Benoist");
-var driver18 = new Driver("Parnelli Jones");
-var driver19 = new Driver("Stefan Bellof");
-var driver20 = new Driver("Keke Rosberg");
-var driver21 = new Driver("Olivier Gendebien");
-var driver22 = new Driver("Nico Rosberg");
-var driver23 = new Driver("Luigi Fagioli");
-var driver24 = new Driver("Denny Hulme");
-var driver25 = new Driver("Bill Vukovich");
-var driver26 = new Driver("Jo Siffert");
-var driver27 = new Driver("Malcolm Campbell");
-var driver28 = new Driver("Louis Chiron");
-var driver29 = new Driver("Felice Nazzaro");
-var driver30 = new Driver("Hermann Lang");
-
-var drivers = new List<Driver>();
-
-drivers.Add(driver1);
-drivers.Add(driver2);
-drivers.Add(driver3);
-drivers.Add(driver4);
-drivers.Add(driver5);
-drivers.Add(driver6);
-drivers.Add(driver7);
-drivers.Add(driver8);
-drivers.Add(driver9);
-drivers.Add(driver10);
-drivers.Add(driver11);
-drivers.Add(driver12);
-drivers.Add(driver13);
-drivers.Add(driver14);
-drivers.Add(driver15);
-drivers.Add(driver16);
-drivers.Add(driver17);
-drivers.Add(driver18);
-drivers.Add(driver19);
-drivers.Add(driver20);
-drivers.Add(driver21);
-drivers.Add(driver22);
-drivers.Add(driver23);
-drivers.Add(driver24);
-drivers.Add(driver25);
-drivers.Add(driver26);
-drivers.Add(driver27);
-drivers.Add(driver28); 
-drivers.Add(driver29);
-drivers.Add(driver30);
-
-
+var allDrivers = new List<Driver>();
 var races = new List<Race>();
-
-races.Add(race1);
-races.Add(race2);
-races.Add(race3);
-
+var waitlists = new List<Queue<Driver>>();
+List<string> tempDriversList = new List<string>();
+string? userMenuChoice;
+bool exit = false;
 var calTable = new Table();
 
-calTable.AddColumns("Race", "Date", "Track", "Driver");
-
-foreach (var race in races)
+void CreateDrivers()
 {
-    if (race.Properties != null)
-    {
-        string? name = race.Properties["Name"]?.ToString();
-        string? date = race.Properties["Date"]?.ToString();
-        string? track = race.Properties["Track"]?.ToString();
-        string? driver = race.Properties["Driver"]?.ToString();
+    allDrivers.Add(new Driver("Al Unser Jr."));
+    allDrivers.Add(new Driver("Ricardo Patrese"));
+    allDrivers.Add(new Driver("Erik Carlsson"));
+    allDrivers.Add(new Driver("Craig Breedlove"));
+    allDrivers.Add(new Driver("Louis Meyer"));
+    allDrivers.Add(new Driver("Rauno Aaltonen"));
+    allDrivers.Add(new Driver("Giuseppe Farina"));
+    allDrivers.Add(new Driver("Richard Burns"));
+    allDrivers.Add(new Driver("David Pearson"));
+    allDrivers.Add(new Driver("Jean Behra"));
+    allDrivers.Add(new Driver("Wolfgang von Trips"));
+    allDrivers.Add(new Driver("Henry Segrave"));
+    allDrivers.Add(new Driver("Gerhard Berger"));
+    allDrivers.Add(new Driver("David Coulthard"));
+    allDrivers.Add(new Driver("Carlos Reutemann"));
+    allDrivers.Add(new Driver("Didier Pironi"));
+    allDrivers.Add(new Driver("Robert Benoist"));
+    allDrivers.Add(new Driver("Parnelli Jones"));
+    allDrivers.Add(new Driver("Stefan Bellof"));
+    allDrivers.Add(new Driver("Keke Rosberg"));
+    allDrivers.Add(new Driver("Olivier Gendebien"));
+    allDrivers.Add(new Driver("Nico Rosberg"));
+    allDrivers.Add(new Driver("Luigi Fagioli"));
+    allDrivers.Add(new Driver("Denny Hulme"));
+    allDrivers.Add(new Driver("Bill Vukovich"));
+    allDrivers.Add(new Driver("Jo Siffert"));
+    allDrivers.Add(new Driver("Malcolm Campbell"));
+    allDrivers.Add(new Driver("Louis Chiron"));
+    allDrivers.Add(new Driver("Felice Nazzaro"));
+    allDrivers.Add(new Driver("Hermann Lang"));
+}
 
-        if (name != null && date != null && track != null && driver != null)
+void CreateRaces()
+{
+    races.Add(new Race("1", "Indianapolis 500", "May 25, 2025", "Indianapolis Motor Speedway"));
+    races.Add(new Race("2", "24 Hours of Le Mans", "June 11, 2025", "Circuit de la Sarthe"));
+    races.Add(new Race("3", "Daytona 500", "February 16, 2025", "Indianapolis Motor Speedway"));
+}
+
+void CreateWaitlists()
+{
+    waitlists.Add(new Queue<Driver>());
+    waitlists.Add(new Queue<Driver>());
+    waitlists.Add(new Queue<Driver>());
+}
+
+void CreateCalTable()
+{
+
+    calTable.AddColumns("ID", "Race", "Date", "Track", "Drivers");
+
+    foreach (var race in races)
+    {
+        if (race.Properties != null)
         {
-            calTable.AddRow(name, date, track, driver);
+            string? raceId = race.Properties["ID"]?.ToString();
+            string? name = race.Properties["Name"]?.ToString();
+            string? date = race.Properties["Date"]?.ToString();
+            string? track = race.Properties["Track"]?.ToString();
+            string? drivers = race.Properties["Drivers"].ToString();
+
+            if (raceId != null && name != null && date != null && track != null && drivers != null)
+            {
+                calTable.AddRow(raceId, name, date, track, drivers);
+            }
         }
+    }
+
+    Console.WriteLine("RACING CALENDAR:");
+    AnsiConsole.Write(calTable);
+}
+
+void CreateDriverTable()
+{
+    Table driverTable = new Table();
+    var driverId = 1;
+
+    driverTable.AddColumns("ID", "Name");
+
+    foreach (var driver in allDrivers)
+    {
+        driverTable.AddRow(driverId.ToString(), driver.Name);
+        driverId++;
+    }
+
+    Console.WriteLine("AVAILABLE DRIVERS:");
+    AnsiConsole.Write(driverTable);
+}
+
+CreateDrivers();
+CreateRaces();
+CreateCalTable();
+CreateDriverTable();
+CreateWaitlists();
+
+Console.WriteLine("Press any key to randomly assign drivers to each race...");
+Console.ReadKey();
+
+Race.AddDrivers(races, allDrivers, waitlists, tempDriversList);
+
+CreateCalTable();
+
+Race.PopulateWaitlists(allDrivers, waitlists, tempDriversList);
+
+
+void CreateWaitlistTable()
+{
+    var waitlistTable = new Table();
+    var raceNum = 0;
+
+    foreach (var race in races)
+    {
+        waitlistTable.AddColumns($"Race {raceNum + 1} Waitlisted Drivers");
+        raceNum++;
+    }
+
+    raceNum = 0;
+    for (int i = 0; i < 5; i++) 
+    {
+        waitlistTable.AddRow(waitlists[raceNum].ElementAt(i).Name, waitlists[raceNum+1].ElementAt(i).Name, waitlists[raceNum+2].ElementAt(i).Name);
+        
+    }
+    
+
+    Console.WriteLine("WAITLIST DRIVERS:");
+    AnsiConsole.Write(waitlistTable);
+}
+
+CreateWaitlistTable();
+
+void PrintMenu()
+{
+    Console.WriteLine("MAIN MENU");
+    Console.WriteLine("1) Add a Driver to a Race");
+    Console.WriteLine("2) Remove a Driver from a Race");
+    Console.WriteLine("3) Exit program");
+}
+
+void GetUserMenuSelection()
+{
+    string[] menuOptions = { "1", "2", "3" };
+    do
+    {
+        Console.WriteLine("What would you like to do? Enter 1, 2, or 3:");
+        userMenuChoice = Console.ReadLine();
+        if (!menuOptions.Contains(userMenuChoice))
+        {
+            Console.WriteLine("That is not a valid option. You must enter 1, 2, or 3.");
+        }
+    } while (!menuOptions.Contains(userMenuChoice));
+}
+
+void MenuSelectionControl(string userMenuChoice)
+{
+    switch (userMenuChoice)
+    {
+        case "1":
+            Console.Clear();
+            CreateCalTable();
+            Race.AddADriver();
+            break;
+        case "2":
+            Console.Clear();
+            CreateCalTable();
+            Race.RemoveADriver(races, calTable);
+            CreateCalTable();
+            break;
+        case "3":
+            Console.WriteLine("\nOK, bye!");
+            exit = true;
+            Environment.Exit(0);
+            break;
     }
 }
 
-Console.WriteLine("RACING CALENDAR:");
-AnsiConsole.Write(calTable);
-
-Table driverTable = new Table();
-var id = 1;
-
-driverTable.AddColumns("ID", "Name");
-
-foreach (var driver in drivers)
+do
 {
-        driverTable.AddRow(id, driver.Name);
-        id++;
-}
+    PrintMenu();
+    GetUserMenuSelection();
+    MenuSelectionControl(userMenuChoice!);
+} while (exit == false);
 
-Console.WriteLine("DRIVERS:");
-AnsiConsole.Write(driverTable);
--- Race.cs
+++ Race.cs
@@ -1,20 +1,143 @@
-﻿public class Race
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Diagnostics;
+using ConsoleTable;
+
+public class Race
 {
-    public Race(string name, string date, string track, string driver)
+    public Race(string raceId, string name, string date, string track)
     {
         Properties = new Dictionary<string, object>
         {
+            { "ID", raceId },
             { "Name", name },
             { "Date", date },
             { "Track", track },
-            { "Driver", driver }
+            { "Drivers", string.Empty }
         };
     }
 
     public Dictionary<string, object> Properties { get; set; }
 
-    public void AddDriver()
+    public static void AddDrivers(List<Race> races, List<Driver> allDrivers, List<Queue<Driver>> waitlists, List<string> tempDriversList)
+    {
+        var random = new Random();
+        Driver? currentDriver = null;
+        int currentDriverIndex = 0;
+
+        foreach (var race in races)
+        {
+            var driverNames = new StringBuilder();
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (race.Properties["Drivers"] == currentDriver)
+                {
+                    i--;
+                    continue;
+                }
+                currentDriver = allDrivers[random.Next(allDrivers.Count)];
+                currentDriverIndex = allDrivers.IndexOf(currentDriver);
+                tempDriversList.Add(currentDriver.Name);
+                allDrivers.RemoveAt(currentDriverIndex);
+                driverNames.Append(currentDriver.Name);
+                driverNames.Append(", ");
+            }
+
+            race.Properties["Drivers"] = driverNames.ToString().TrimEnd(',', ' ');
+        }  
+    }
+
+    public static void PopulateWaitlists(List<Driver> allDrivers, List<Queue<Driver>> waitlists, List<string> tempDriversList)
+    {
+        var raceNum = 0;
+        foreach (var driver in allDrivers)
+        {
+            if (!tempDriversList.Contains(driver.Name))
+            {
+                if (waitlists[raceNum].Count < 5)
+                {
+                    waitlists[raceNum].Enqueue(driver);
+                }
+                else if (waitlists[raceNum + 1].Count < 5)
+                {
+                    waitlists[raceNum + 1].Enqueue(driver);
+                }
+                else
+                {
+                    waitlists[raceNum + 2].Enqueue(driver);
+                }
+            }
+        }
+    }
+
+    public static void AddADriver()
     {
 
     }
+
+    public static void RemoveADriver(List<Race> races, Spectre.Console.Table calTable)
+    {
+        Console.WriteLine("Enter the ID of the race you want to remove a driver from:");
+
+        string[] raceIdOptions = { "1", "2", "3" };
+        string? raceIdInput;
+        do
+        {
+            raceIdInput = Console.ReadLine();
+            if (!raceIdOptions.Contains(raceIdInput))
+            {
+                Console.WriteLine("That is not a valid option. You must enter 1, 2, or 3.");
+            }
+        } while (!raceIdOptions.Contains(raceIdInput));
+
+        string? driverName;
+        TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+        switch (raceIdInput)
+        {
+            case "1":
+                Console.WriteLine("Enter the name of the driver you want to remove from Race 1");
+                driverName = Console.ReadLine();
+                driverName = driverName!.Trim();
+                driverName = textInfo.ToTitleCase(driverName);
+                if (races[0].Properties["Drivers"].ToString()!.Split(new char[] { ',' }, StringSplitOptions.None).Any(name => name.Trim().Equals(driverName, StringComparison.OrdinalIgnoreCase)))
+                {
+                    string driverNames = races[0].Properties["Drivers"].ToString();
+                    races[0].Properties["Drivers"] = "";
+                    Console.WriteLine($"{driverName} has been removed.");
+
+
+                    
+                    driverNames = races[0].Properties["Drivers"].ToString();
+
+                    foreach (var property in races[0].Properties)
+                    {
+                        if (races[0].Properties["Drivers"].ToString() != driverName)
+                        {
+                            driverNames?.Append(driverName);
+                            driverNames?.Append(", ");
+                        }
+                    }
+
+                    races[0].Properties["Drivers"] = driverNames.ToString().TrimEnd(',', ' ');
+                }
+                else
+                {
+                    Console.WriteLine($"{driverName} is not listed in this race.");
+                }
+                break;
+            case "2":
+                
+                break;
+            case "3":
+                
+                break;
+        }
+    }
+
+    
 }
+
+
